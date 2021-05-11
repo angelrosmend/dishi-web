4b47709306f98f038cf79f3f9c0f72f21354b883
angelrosmend <angelrosmend@gmail.com>
2021-05-10 23:05:30 -0300

    added tootips on cardproduct component

--- src/components/tarjetas/home/producto/components/BtnAdd.jsx
+++ src/components/tarjetas/home/producto/components/BtnAdd.jsx
@@ -1,25 +1,54 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { Fragment } from 'react'
 import { CartContext } from '../../../../../context/CarritoContext'
+import { Overlay} from "react-bootstrap"
+import CustomTooltip from '../../../../tooltip/CustomTooltip'
 
 function BtnAdd(props) {
     const {stock, inCart, showBtnQty, hideBtnQty, prodInfo} = props
     const {CART_METHODS} = useContext(CartContext)
     const {removeFromCart, addToCart} = CART_METHODS
+
+
+    const [showTooltip, setShowShowTooltip] = useState(false);
+    const [showTooltipAdd, setShowShowTooltipAdd] = useState(false);
+    const toggleTooltip = () => setShowShowTooltip(!showTooltip)
+    const toggleTooltipAdd = () => setShowShowTooltipAdd(!showTooltipAdd)
+
+    const targetAdd = useRef(null);
+    const targetRemove  = useRef(null)
     return (
         <Fragment>
              {//stock &&
               inCart ? 
+              <Fragment>
                 <i onClick={()=> removeFromCart({...prodInfo})}
                    className="fas fa-cart-plus btn-cart-prod cart-active"
-                   onMouseEnter={showBtnQty}
-                   onMouseOver={showBtnQty}
+                   onMouseEnter={()=> {showBtnQty(); toggleTooltip(); }}
+                   onMouseLeave={toggleTooltip}
+                   ref={targetRemove}
                    />
+                  
+              <CustomTooltip target={targetRemove}
+                             show={showTooltip}
+                             placement="left">
+                      Remover del<br /> carrito
+              </CustomTooltip>
+              </Fragment>
                      :
+              <Fragment>
                 <i onClick={()=> {addToCart({...prodInfo}); showBtnQty()}}
+                   onMouseEnter={()=> {showBtnQty(); toggleTooltipAdd(); }}
+                   onMouseLeave={toggleTooltip}
                    className="fas fa-cart-plus btn-cart-prod"
-                   
-                   />  
+                   ref={targetAdd}
+                   />
+                <CustomTooltip target={targetAdd}
+                               show={showTooltipAdd}
+                               placement="left">
+                      Agregar al<br /> carrito
+                </CustomTooltip>
+              </Fragment>
              }
         </Fragment>
     )
---
+++ src/components/tooltip/CustomTooltip.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Overlay} from "react-bootstrap"
+
+function CustomTooltip({text, target, show, placement, children}) {
+    return (
+        <Overlay target={target.current} show={show} placement={placement}>
+        {({ placement, arrowProps, show: _show, popper, ...props }) => (
+          <div
+            {...props}
+            style={{
+              backgroundColor: '#82b4ef',
+              padding: '2px 10px',
+              marginRight: "10px",
+              color: 'white',
+              fontSize: "0.8rem",
+              fontWeight:"bold",
+              borderRadius: "20px",
+              ...props.style,
+            }}>
+            {children}
+          </div>
+        )}
+      </Overlay>
+    )
+}
+
+export default CustomTooltip
